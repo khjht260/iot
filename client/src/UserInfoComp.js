41f5e3b790919152dd2abf7c439c9ce1ba6ec292
raywu0123 <b05901189@ntu.edu.tw>
2018-08-11 10:27:49 -0400

    chinese version

--- client/src/UserInfoComp.js
+++ client/src/UserInfoComp.js
@@ -7,19 +7,21 @@ class SignInComp extends Component {
     if (!this.props.permission) return <div></div>
     return (
       <div>
-        <h3> {this.props.data.username} </h3>
-        <h3> progress: {this.props.data.num_progress} </h3>
-        <h3> spent   : {this.props.data.num_spent} </h3>
-        <h3> credits : {credits}</h3>
-        <button 
+        <h3> 帳號：{this.props.data.username}學霸幣總額: {this.props.data.num_progress*10} 元 已使用金額 : {this.props.data.num_spent*10}元</h3>
+        <h3> </h3>
+        <h3> </h3>
+        <h3> 可使用金額 : {credits*10}元</h3>
+        <h3> 按下下方投幣鍵，會扣10元學霸幣，玩籃球機一次，系統會自動幫你登出</h3>
+        <h3> 若不想玩，按下方登出鍵登出，不會扣款</h3>
+        <button className="myButton"
           id="activateBtn"
           onClick={this.props.handleActivate}
           disabled={!credits > 0}
-        > ACTIVATE </button>
+        > 投  幣 </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-        <button
+        <button className="myButton"
           onClick={this.props.handleSignOut}
-        > SIGN OUT </button>
+        > 登  出 </button>
       </div>
     );
   }
